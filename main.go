98ae6b7a698ebb84ee65079086a1f024b363b4e0
Kananek Thongkam <info.dvgamer@gmail.com>
2022-06-06 00:52:57 +0700

    [Update] filenames

--- main.go
+++ main.go
@@ -102,25 +102,3 @@ func main() {
 		log.Fatalf("DB: %s", err)
 	}
 }
-
-func appFiberListen(app *fiber.App, port string) {
-	log.Printf("Fiber Started at '%s'\n", port)
-	if err := app.Listen(port); err != nil {
-		log.Fatalf("listen: %+v", err)
-	}
-}
-
-func handerMiddlewareSecurity(c *fiber.Ctx) error {
-	// Set some security headers:
-	c.Set("X-XSS-Protection", "1; mode=block")
-	c.Set("X-Content-Type-Options", "nosniff")
-	c.Set("X-Download-Options", "noopen")
-	c.Set("Strict-Transport-Security", "max-age=5184000")
-	c.Set("X-Frame-Options", "SAMEORIGIN")
-	c.Set("X-DNS-Prefetch-Control", "off")
-	return c.Next()
-}
-
-func handlerHealth(c *fiber.Ctx) error {
-	return c.SendString("☕")
-}
